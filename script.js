d7d1a2a305e43ff7192b641490658ffd060df129
Samuel Adzoho <edinamsam4@gmail.com>
2026-01-13 17:45:52 +0000

    Implement number input and display logic as well as minor keypad alteration

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 const keypad = document.querySelector(".keypad");
+const display = document.querySelector(".display-text");
+
+let displayValue = "0";
+
+function updateDisplay() {
+  display.textContent = displayValue;
+}
 
 keypad.addEventListener("click", (e) => {
   if (!e.target.matches("button")) return;
 
   const value = e.target.textContent;
-  console.log(value);
+
+  if (!isNaN(value)) {
+    handleNumber(value);
+  }
 });
+
+function handleNumber(number) {
+  if (displayValue === "0") {
+    displayValue = number;
+  } else {
+    displayValue += number;
+  }
+
+  updateDisplay();
+}
